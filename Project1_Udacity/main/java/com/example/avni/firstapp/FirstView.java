6d1dd63b3e3a5195be22e3e372dae08f1f1e904c
shilpamahendriker <37084465+shilpamahendriker@users.noreply.github.com>
2018-04-11 11:06:20 -0400

    Add files via upload

---
+++ Project1_Udacity/main/java/com/example/avni/firstapp/FirstView.java
@@ -0,0 +1,23 @@
+package com.example.avni.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /*protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textview = new TextView(this);
+        textview.setText("Wow!!");
+        textview.setTextSize(80);
+
+
+        setContentView(textview);
+    }*/
+}
